7ebea1b76e3862832071063780043eee73dd4bfb
cuongdc <duongcuong96@gmail.com>
2021-07-30 14:28:38 +0700

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -98159,7 +98159,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62626" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62353" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
